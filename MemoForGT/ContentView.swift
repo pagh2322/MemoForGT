f12fccb8a33a06947551b277466dc8aca430665c
pagh2322 <pagh2322@naver.com>
2022-04-30 19:51:33 +0900

    Initialize

---
+++ MemoForGT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoForGT
+//
+//  Created by peo on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoForGT/MemoForGTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoForGTApp.swift
+//  MemoForGT
+//
+//  Created by peo on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct MemoForGTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
